b34f21af45db1e905efad6abb87338b6c74382ca
prafessor <olegmilyo@gmail.com>
2025-04-26 18:35:01 +0300

    fix basic 5 and generic 1

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -9,7 +9,7 @@ enum DayOfWeek {
 }
 
 const isWeekend = (day: DayOfWeek): boolean => {
-  if (day > 4) {
+  if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) {
     return true;
   } else {
     return false;
